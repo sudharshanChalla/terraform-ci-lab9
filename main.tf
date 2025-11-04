236eb316bdc4af00d9662d14c214fd4279a549a5
sudharshanChalla <sudharshanchalla1999@gmail.com>
2025-11-03 22:39:17 -0500

    Step 2: Add minimal AWS S3 bucket resource

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
-resource "random_pet" "example" {
-  length = 2
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "lab_bucket" {
+  bucket = "lab-terraform-ci-example-${var.yourid4}"
+  acl    = "private"
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+variable "yourid4" {
+  type    = string
+  default = "9711"
 }
